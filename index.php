411c98f4aa83f4b5dd5af2471c7f0bac6a13f759
Heriberto41 <1715110167@utec-tgo.edu.mx>
2017-04-01 16:26:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $hostname_localhost ="if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
 $database_localhost ="eu3q05rxqr8smuf5";//Nombre de nuestra base de datos
 $username_localhost ="wrnswivptaf9lfgp";//Nombre de usuario de nuestra base de datos (yo utilizo el valor por defecto)
 $password_localhost ="ini3x564w4hi3nq6";//Contraseña de nuestra base de datos (yo utilizo por defecto)
-$localhost = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost)//Conexión a nuestro servidor mysql
+$localhost = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost);//Conexión a nuestro servidor mysql
 or
 trigger_error(mysqli_error(),E_USER_ERROR); //mensaaje de error si no se puede conectar
 mysqli_select_db($database_localhost, $localhost);//seleccion de la base de datos con la qu se desea trabajar
